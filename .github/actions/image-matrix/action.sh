293c432d303f4da329bcc802c53c2b254032af19
ptaylor <paul.e.taylor@me.com>
2023-06-05 16:27:33 -0700

    temporarily ignore building images when utils feature changes

--- .github/actions/image-matrix/action.sh
+++ .github/actions/image-matrix/action.sh
@@ -16,7 +16,8 @@ files="${@:3}";
 
 # Select all images that use a changed feature
 
-common_features="cpp|gitlab-cli";
+# common_features="cpp|gitlab-cli";
+common_features="";
 
 features="$(echo                        \
   "${features}"  `# ["foo", "bar"]`     \
